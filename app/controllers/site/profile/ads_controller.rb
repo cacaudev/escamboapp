f0d5e04e93b46b978368ada729a0d56475612da5
cacaudev <cacauabreu2013@gmail.com>
2018-11-02 22:59:21 -0300

    Atualizado ate aula 62 - modulo 3

--- app/controllers/site/profile/ads_controller.rb
+++ app/controllers/site/profile/ads_controller.rb
@@ -1,5 +1,15 @@
 class Site::Profile::AdsController < Site::ProfileController
+  before_action :set_ad, only: [:edit]
+
   def index
     @ads = Ad.ads_by_current_member(current_member)
   end
+
+  def edit    
+  end
+
+  private
+    def set_ad
+      @ad = Ad.find(params[:id])
+    end
 end
